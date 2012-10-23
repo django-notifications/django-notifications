6d65205c6940fcd6fcdefc1b91512502968914fb
Matthew Schinckel <matt@schinckel.net>
2012-10-24 09:41:52 +1030

    Fixed a bug where passing recipients was not really effective: it still marked all as read/unread based on the entire query set.

--- notifications/models.py
+++ notifications/models.py
@@ -23,16 +23,18 @@ class NotificationQuerySet(models.query.QuerySet):
         "Return only unread items in the current queryset"
         return self.filter(unread=True)
     
-    # Should we return self on these?    
+    # Should we return self on these?
     def mark_all_as_read(self, recipient=None):
         if recipient:
             self.filter(recipient=recipient).update(unread=False)
-        self.update(unread=False)
+        els:
+            self.update(unread=False)
     
     def mark_all_as_unread(self, recipient=None):
         if recipient:
             self.filter(recipient=recipient).update(unread=True)
-        self.update(unread=True)
+        else:
+            self.update(unread=True)
 
 class Notification(models.Model):
     """
