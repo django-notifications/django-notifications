c095bda717394d9242d059a9c63f442f8dd84046
Matthew Schinckel <matt@schinckel.net>
2012-10-23 17:22:19 +1030

    Reformat lines.

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,4 +1,6 @@
 from django.dispatch import Signal
 
-notify = Signal(providing_args=['recipient', 'actor', 'verb', 'action_object', 'target',
-    'description', 'timestamp'])
+notify = Signal(providing_args=[
+    'recipient', 'actor', 'verb', 'action_object', 'target', 'description',
+    'timestamp'
+])
