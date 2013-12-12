a87043b5b076a10a1e3393183a003a0805a094ea
Sandro Rodrigues <srtabs@gmail.com>
2013-12-12 01:10:34 +0000

    Added url to mark as unread too
    Forgot to pull that when added mark as unread on view

--- notifications/urls.py
+++ notifications/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('notifications.views',
     url(r'^unread/$', 'unread', name='unread'),
     url(r'^mark-all-as-read/$', 'mark_all_as_read', name='mark_all_as_read'),
     url(r'^mark-as-read/(?P<slug>\d+)/$', 'mark_as_read', name='mark_as_read'),
+    url(r'^mark-as-unread/(?P<slug>\d+)/$', 'mark_as_unread', name='mark_as_unread'),
 )
