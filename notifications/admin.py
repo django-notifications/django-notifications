3dae324d5eb1a72085bb641d96eaca4a55dacdeb
Axel Díaz <diaz.axelio@gmail.com>
2014-05-25 06:03:55 +0000

    Adding filters and displays to model admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from .models import Notification
 
 class NotificationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('recipient', 'actor',
+                    'level', 'target', 'unread', 'public')
+    list_filter = ('level', 'unread', 'public', 'timestamp', )
 
 admin.site.register(Notification, NotificationAdmin)
