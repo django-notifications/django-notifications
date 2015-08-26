b93c75d710e75bf28bf3f007251725195ec7c945
Shezad Khan <shezad@letolab.com>
2015-08-26 16:40:52 +0100

    Add missing migration for Notification model

---
+++ notifications/migrations/0004_auto_20150826_1508.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import notifications.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('notifications', '0003_notification_data'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='notification',
+            name='timestamp',
+            field=models.DateTimeField(default=notifications.models.now),
+        ),
+    ]
