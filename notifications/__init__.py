917f2f8831cb1c6eb5f5d39eb0190699978268cb
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2015-12-09 14:41:16 +0100

    removed old code

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,13 +1,2 @@
 default_app_config = 'notifications.apps.Config'
-# urls = None
-# notify = None
-# try:
-#     from notifications.signals import notify
-# except ImportError:
-#     pass
-# 
-# try:
-#     from notifications.urls import urlpatterns
-#     urls = (urlpatterns, 'notifications', 'notifications')
-# except ImportError:
-#     pass
+
