2b389592462b4e51077f0e68339896b6b54bd2af
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2015-12-09 14:30:34 +0100

    django1.9 passing tests again

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,10 +1,13 @@
-try:
-    from notifications.signals import notify
-except ImportError:
-    pass
-
-try:
-    from notifications.urls import urlpatterns
-    urls = (urlpatterns, 'notifications', 'notifications')
-except ImportError:
-    pass
+default_app_config = 'notifications.apps.Config'
+# urls = None
+# notify = None
+# try:
+#     from notifications.signals import notify
+# except ImportError:
+#     pass
+# 
+# try:
+#     from notifications.urls import urlpatterns
+#     urls = (urlpatterns, 'notifications', 'notifications')
+# except ImportError:
+#     pass
