67d84dcaae97c1e303d3a0be5968e7ce82d5c7cc
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2018-05-31 00:02:45 -0300

    Improve code quality on signals.py

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,6 +1,7 @@
+''' Django notifications signal file '''
 from django.dispatch import Signal
 
-notify = Signal(providing_args=[
+notify = Signal(providing_args=[  # pylint: disable=invalid-name
     'recipient', 'actor', 'verb', 'action_object', 'target', 'description',
     'timestamp', 'level'
 ])
