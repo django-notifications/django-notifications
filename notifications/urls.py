51677dd83654eb1f457612f1d4e04f20e59ee89a
Juliano Gouveia <juliano@neosacode.com>
2018-06-13 18:30:28 -0300

    Fix url pattern alias for the re_path django recommended function

--- notifications/urls.py
+++ notifications/urls.py
@@ -7,22 +7,22 @@ from django import get_version
 from . import views
 
 if StrictVersion(get_version()) >= StrictVersion('2.0'):
-    from django.urls import path as pattern
+    from django.urls import re_path as pattern
 else:
     from django.conf.urls import url as pattern
 
 
 urlpatterns = [
-    pattern('', views.AllNotificationsList.as_view(), name='all'),
-    pattern('unread/', views.UnreadNotificationsList.as_view(), name='unread'),
-    pattern('mark-all-as-read/', views.mark_all_as_read, name='mark_all_as_read'),
-    pattern('mark-as-read/<slug:slug>/', views.mark_as_read, name='mark_as_read'),
-    pattern('mark-as-unread/<slug:slug>/', views.mark_as_unread, name='mark_as_unread'),
-    pattern('delete/<slug:slug>/', views.delete, name='delete'),
-    pattern('api/unread_count/', views.live_unread_notification_count, name='live_unread_notification_count'),
-    pattern('api/all_count/', views.live_all_notification_count, name='live_all_notification_count'),
-    pattern('api/unread_list/', views.live_unread_notification_list, name='live_unread_notification_list'),
-    pattern('api/all_list/', views.live_all_notification_list, name='live_all_notification_list'),
+    pattern(r'^$', views.AllNotificationsList.as_view(), name='all'),
+    pattern(r'^unread/$', views.UnreadNotificationsList.as_view(), name='unread'),
+    pattern(r'^mark-all-as-read/$', views.mark_all_as_read, name='mark_all_as_read'),
+    pattern(r'^mark-as-read/(?P<slug>\d+)/$', views.mark_as_read, name='mark_as_read'),
+    pattern(r'^mark-as-unread/(?P<slug>\d+)/$', views.mark_as_unread, name='mark_as_unread'),
+    pattern(r'^delete/(?P<slug>\d+)/$', views.delete, name='delete'),
+    pattern(r'^api/unread_count/$', views.live_unread_notification_count, name='live_unread_notification_count'),
+    pattern(r'^api/all_count/$', views.live_all_notification_count, name='live_all_notification_count'),
+    pattern(r'^api/unread_list/$', views.live_unread_notification_list, name='live_unread_notification_list'),
+    pattern(r'^api/all_list/', views.live_all_notification_list, name='live_all_notification_list'),
 ]
 
 app_name = 'notifications'
