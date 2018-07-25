03696c70ebfe5d03e83d16366a53730ccbd98661
Dan DeFelippi <dan@driverdan.com>
2018-07-25 11:09:32 -0500

    Add support to register_notify_callbacks for marking notifications as read.

--- notifications/static/notifications/notify.js
+++ notifications/static/notifications/notify.js
@@ -5,13 +5,15 @@ var notify_fetch_count;
 var notify_unread_url;
 var notify_mark_all_unread_url;
 var notify_refresh_period = 15000;
+// Set notify_mark_as_read to true to mark notifications as read when fetched
+var notify_mark_as_read = false;
 var consecutive_misfires = 0;
 var registered_functions = [];
 
 function fill_notification_badge(data) {
     var badges = document.getElementsByClassName(notify_badge_class);
     if (badges) {
-        for(var i = 0; i < badges.length; i++){
+        for(var i = 0; i < badges.length; i++) {
             badges[i].innerHTML = data.unread_count;
         }
     }
@@ -22,22 +24,23 @@ function fill_notification_list(data) {
     if (menus) {
         var messages = data.unread_list.map(function (item) {
             var message = "";
-            if(typeof item.actor !== 'undefined'){
+
+            if (typeof item.actor !== 'undefined') {
                 message = item.actor;
             }
-            if(typeof item.verb !== 'undefined'){
+            if (typeof item.verb !== 'undefined') {
                 message = message + " " + item.verb;
             }
-            if(typeof item.target !== 'undefined'){
+            if (typeof item.target !== 'undefined') {
                 message = message + " " + item.target;
             }
-            if(typeof item.timestamp !== 'undefined'){
+            if (typeof item.timestamp !== 'undefined') {
                 message = message + " " + item.timestamp;
             }
             return '<li>' + message + '</li>';
         }).join('')
 
-        for (var i = 0; i < menus.length; i++){
+        for (var i = 0; i < menus.length; i++) {
             menus[i].innerHTML = messages;
         }
     }
@@ -48,31 +51,37 @@ function register_notifier(func) {
 }
 
 function fetch_api_data() {
+    // only fetch data if a function is setup
     if (registered_functions.length > 0) {
-        //only fetch data if a function is setup
         var r = new XMLHttpRequest();
-        r.addEventListener('readystatechange', function(event){
-            if (this.readyState === 4){
-                if (this.status === 200){
+        var params = '?max=' + notify_fetch_count;
+
+        if (notify_mark_as_read) {
+            params += '&mark_as_read=true';
+        }
+
+        r.addEventListener('readystatechange', function(event) {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
                     consecutive_misfires = 0;
                     var data = JSON.parse(r.responseText);
-                    for(var i = 0; i < registered_functions.length; i++) {
+                    for (var i = 0; i < registered_functions.length; i++) {
                        registered_functions[i](data);
                     }
-                }else{
+                } else {
                     consecutive_misfires++;
                 }
             }
-        })
-        r.open("GET", notify_api_url+'?max='+notify_fetch_count, true);
+        });
+        r.open("GET", notify_api_url + params, true);
         r.send();
     }
     if (consecutive_misfires < 10) {
-        setTimeout(fetch_api_data,notify_refresh_period);
+        setTimeout(fetch_api_data, notify_refresh_period);
     } else {
         var badges = document.getElementsByClassName(notify_badge_class);
         if (badges) {
-            for (var i = 0; i < badges.length; i++){
+            for (var i = 0; i < badges.length; i++) {
                 badges[i].innerHTML = "!";
                 badges[i].title = "Connection lost!"
             }
