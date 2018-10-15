9896a4c5d6f2a023ad47b506a566cba175bfbf2b
Davide Setti <d.setti@certego.net>
2018-10-15 16:50:02 +0200

    Remove deprecated view for django 2.x tests

--- notifications/tests/urls.py
+++ notifications/tests/urls.py
@@ -4,20 +4,21 @@ from distutils.version import StrictVersion  # pylint: disable=no-name-in-module
 
 from django import get_version
 from django.contrib import admin
-from django.contrib.auth.views import login
 from notifications.tests.views import (live_tester,  # pylint: disable=no-name-in-module,import-error
                                        make_notification)
 
 if StrictVersion(get_version()) >= StrictVersion('2.0'):
+    from django.contrib.auth.views import LoginView
     from django.urls import include, path  # noqa
     urlpatterns = [
         path('test_make/', make_notification),
         path('test/', live_tester),
-        path('login/', login, name='login'),  # reverse for django login is not working
+        path('login/', LoginView.as_view(), name='login'),  # reverse for django login is not working
         path('admin/', admin.site.urls),
         path('', include('notifications.urls', namespace='notifications')),
     ]
 else:
+    from django.contrib.auth.views import login
     from django.conf.urls import include, url
     urlpatterns = [
         url(r'^login/$', login, name='login'),  # reverse for django login is not working
