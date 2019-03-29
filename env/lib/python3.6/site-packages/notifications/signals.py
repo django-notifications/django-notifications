d7e4af620a37af7bfbf99819237302b1555c2065
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-03-29 15:13:02 +0300

    remove unneccesry files

--- env/lib/python3.6/site-packages/notifications/signals.py
+++
@@ -1,8 +0,0 @@
-''' Django notifications signal file '''
-# -*- coding: utf-8 -*-
-from django.dispatch import Signal
-
-notify = Signal(providing_args=[  # pylint: disable=invalid-name
-    'recipient', 'actor', 'verb', 'action_object', 'target', 'description',
-    'timestamp', 'level'
-])
