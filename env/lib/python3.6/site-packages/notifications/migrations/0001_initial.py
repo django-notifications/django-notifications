ebbf3dde0992a85c774998cb00785a7eb82e3021
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-03-29 15:11:06 +0300

    humanize_timestamp function split.Create naturalday() and naturaltime() function for humanize timestmap.

---
+++ env/lib/python3.6/site-packages/notifications/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Notification',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('level', models.CharField(default='info', max_length=20, choices=[('success', 'success'), ('info', 'info'), ('warning', 'warning'), ('error', 'error')])),
+                ('unread', models.BooleanField(default=True)),
+                ('actor_object_id', models.CharField(max_length=255)),
+                ('verb', models.CharField(max_length=255)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('target_object_id', models.CharField(max_length=255, null=True, blank=True)),
+                ('action_object_object_id', models.CharField(max_length=255, null=True, blank=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('public', models.BooleanField(default=True)),
+                ('action_object_content_type', models.ForeignKey(related_name='notify_action_object', blank=True, to='contenttypes.ContentType', null=True, on_delete=models.CASCADE)),
+                ('actor_content_type', models.ForeignKey(related_name='notify_actor', to='contenttypes.ContentType', on_delete=models.CASCADE)),
+                ('recipient', models.ForeignKey(related_name='notifications', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
+                ('target_content_type', models.ForeignKey(related_name='notify_target', blank=True, to='contenttypes.ContentType', null=True, on_delete=models.CASCADE)),
+            ],
+            options={
+                'ordering': ('-timestamp',),
+            },
+            bases=(models.Model,),
+        ),
+    ]
