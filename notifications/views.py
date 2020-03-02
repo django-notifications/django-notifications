8d9751c372d123bebe649469c7b461acd32debf8
nahid <nahid@misfit.tech>
2020-03-02 12:31:19 +0600

    Add helpers.py file with some helper functions to ensure DRY principle

--- notifications/views.py
+++ notifications/views.py
@@ -4,14 +4,13 @@ from distutils.version import StrictVersion  # pylint: disable=no-name-in-module
 
 from django import get_version
 from django.contrib.auth.decorators import login_required
-from django.forms import model_to_dict
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 from notifications import settings
 from notifications.models import Notification
-from notifications.utils import id2slug, slug2id
-from notifications.settings import get_config
+from notifications.utils import slug2id
+from notifications.helpers import get_notification_list
 from django.views.decorators.cache import never_cache
 
 if StrictVersion(get_version()) >= StrictVersion('1.7.0'):
@@ -157,32 +156,8 @@ def live_unread_notification_list(request):
         }
         return JsonResponse(data)
 
-    default_num_to_fetch = get_config()['NUM_TO_FETCH']
-    try:
-        # If they don't specify, make it 5.
-        num_to_fetch = request.GET.get('max', default_num_to_fetch)
-        num_to_fetch = int(num_to_fetch)
-        if not (1 <= num_to_fetch <= 100):
-            num_to_fetch = default_num_to_fetch
-    except ValueError:  # If casting to an int fails.
-        num_to_fetch = default_num_to_fetch
+    unread_list = get_notification_list(request, 'unread')
 
-    unread_list = []
-
-    for notification in request.user.notifications.unread()[0:num_to_fetch]:
-        struct = model_to_dict(notification)
-        struct['slug'] = id2slug(notification.id)
-        if notification.actor:
-            struct['actor'] = str(notification.actor)
-        if notification.target:
-            struct['target'] = str(notification.target)
-        if notification.action_object:
-            struct['action_object'] = str(notification.action_object)
-        if notification.data:
-            struct['data'] = notification.data
-        unread_list.append(struct)
-        if request.GET.get('mark_as_read'):
-            notification.mark_as_read()
     data = {
         'unread_count': request.user.notifications.unread().count(),
         'unread_list': unread_list
@@ -205,32 +180,8 @@ def live_all_notification_list(request):
         }
         return JsonResponse(data)
 
-    default_num_to_fetch = get_config()['NUM_TO_FETCH']
-    try:
-        # If they don't specify, make it 5.
-        num_to_fetch = request.GET.get('max', default_num_to_fetch)
-        num_to_fetch = int(num_to_fetch)
-        if not (1 <= num_to_fetch <= 100):
-            num_to_fetch = default_num_to_fetch
-    except ValueError:  # If casting to an int fails.
-        num_to_fetch = default_num_to_fetch
+    all_list = get_notification_list(request)
 
-    all_list = []
-
-    for notification in request.user.notifications.all()[0:num_to_fetch]:
-        struct = model_to_dict(notification)
-        struct['slug'] = id2slug(notification.id)
-        if notification.actor:
-            struct['actor'] = str(notification.actor)
-        if notification.target:
-            struct['target'] = str(notification.target)
-        if notification.action_object:
-            struct['action_object'] = str(notification.action_object)
-        if notification.data:
-            struct['data'] = notification.data
-        all_list.append(struct)
-        if request.GET.get('mark_as_read'):
-            notification.mark_as_read()
     data = {
         'all_count': request.user.notifications.count(),
         'all_list': all_list
