b10c0ae38acdcc69855e25b43c1906f642b8898b
Sny <sny@blockgrain.com.au>
2020-03-03 15:18:52 +0530

    Removed jsonfield dep | this makes the package compatible with postgres only

--- notifications/migrations/0003_notification_data.py
+++ notifications/migrations/0003_notification_data.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import jsonfield.fields
+from django.contrib.postgres.fields import JSONField
 
 
 class Migration(migrations.Migration):
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='notification',
             name='data',
-            field=jsonfield.fields.JSONField(null=True, blank=True),
+            field=JSONField(null=True, blank=True),
             preserve_default=True,
         ),
     ]
