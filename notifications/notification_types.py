04d8cace14a8fad68eedc8f9f9fc82bee2370357
Timothy Hobbs <timothyhobbs@seznam.cz>
2020-03-27 21:06:26 +0100

    Add update method to notification type abc

--- notifications/notification_types.py
+++ notifications/notification_types.py
@@ -3,7 +3,18 @@ import swapper
 NotificationTemplate = swapper.load_model("notifications", "NotificationTemplate")
 
 
-class NotificationType:
+class NotificationType(AbstractClass):
+    @abstractmethod
+    def populate():
+        pass
+
+    @abstractmethod
+    def check_condition(self, recipient):
+        """
+        Returns true if the notification is applicable to the recipient.
+        """
+        pass
+
     @property
     def template(self):
         template, created = NotificationTemplate.objects.get_or_create(slug=self.slug)
@@ -17,3 +28,9 @@ class NotificationType:
 
     def revoke(self, recipient):
         recipient.revoke_templated_notification(self.template)
+
+    def update(self, recipient):
+        if self.check_condition(recipient):
+            self.send(recipient)
+        else:
+            self.revoke(recipient)
