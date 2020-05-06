703f901957f60336f392bd33e924437989c936d0
s0th1nk <38642167+s0th1nk@users.noreply.github.com>
2020-05-06 23:43:21 +0700

    Mark selected notifications as unread in admin
    This is helpful when you want to resend notification.

--- notifications/admin.py
+++ notifications/admin.py
@@ -5,12 +5,16 @@ from swapper import load_model
 
 Notification = load_model('notifications', 'Notification')
 
+def mark_unread(modeladmin, request, queryset):
+    queryset.update(unread=True)
+mark_unread.short_description = "Mark selected notifications as unread"
 
 class NotificationAdmin(admin.ModelAdmin):
     raw_id_fields = ('recipient',)
     list_display = ('recipient', 'actor',
                     'level', 'target', 'unread', 'public')
     list_filter = ('level', 'unread', 'public', 'timestamp',)
+    actions = [mark_unread]
 
     def get_queryset(self, request):
         qs = super(NotificationAdmin, self).get_queryset(request)
