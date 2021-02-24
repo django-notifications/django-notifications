c53a0beddff9962c8ba85a9d4ac93925dc642caa
amir <amirreza.m.saki@gmail.com>
2021-02-24 15:52:28 +0330

    Modify urls patterns for api.

--- notifications/urls.py
+++ notifications/urls.py
@@ -13,16 +13,14 @@ else:
 
 
 urlpatterns = [
-    pattern(r'^$', views.AllNotificationsList.as_view(), name='all'),
-    pattern(r'^unread/$', views.UnreadNotificationsList.as_view(), name='unread'),
     pattern(r'^mark-all-as-read/$', views.mark_all_as_read, name='mark_all_as_read'),
     pattern(r'^mark-as-read/(?P<slug>\d+)/$', views.mark_as_read, name='mark_as_read'),
     pattern(r'^mark-as-unread/(?P<slug>\d+)/$', views.mark_as_unread, name='mark_as_unread'),
     pattern(r'^delete/(?P<slug>\d+)/$', views.delete, name='delete'),
-    pattern(r'^api/unread_count/$', views.live_unread_notification_count, name='live_unread_notification_count'),
-    pattern(r'^api/all_count/$', views.live_all_notification_count, name='live_all_notification_count'),
-    pattern(r'^api/unread_list/$', views.live_unread_notification_list, name='live_unread_notification_list'),
-    pattern(r'^api/all_list/', views.live_all_notification_list, name='live_all_notification_list'),
+    pattern(r'^unread_count/$', views.live_unread_notification_count, name='live_unread_notification_count'),
+    pattern(r'^all_count/$', views.live_all_notification_count, name='live_all_notification_count'),
+    pattern(r'^unread_list/$', views.live_unread_notification_list, name='live_unread_notification_list'),
+    pattern(r'^all_list/', views.live_all_notification_list, name='live_all_notification_list'),
 ]
 
 app_name = 'notifications'
