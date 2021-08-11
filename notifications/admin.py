bb434ef508bf0ca9aac3017d24ccbdd0c8a4ce14
Ian Campbell <ipcampbell@gmail.com>
2021-08-11 17:21:02 -0400

    Remove duplicate code in NotificationAdmin inherited from AbstractNotificationAdmin.

--- notifications/admin.py
+++ notifications/admin.py
@@ -8,14 +8,7 @@ Notification = load_model('notifications', 'Notification')
 
 
 class NotificationAdmin(AbstractNotificationAdmin):
-    raw_id_fields = ('recipient',)
-    list_display = ('recipient', 'actor',
-                    'level', 'target', 'unread', 'public')
-    list_filter = ('level', 'unread', 'public', 'timestamp',)
-
-    def get_queryset(self, request):
-        qs = super(NotificationAdmin, self).get_queryset(request)
-        return qs.prefetch_related('actor')
+    pass
 
 
 admin.site.register(Notification, NotificationAdmin)
