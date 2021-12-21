dea5f04adad4e266b02eb718fa85029226ff64c5
Gagan Deep <pandafy.dev@gmail.com>
2021-12-21 07:08:28 +0530

    Added default auto field in application config

--- notifications/apps.py
+++ notifications/apps.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class Config(AppConfig):
     name = "notifications"
     verbose_name = _("Notifications")
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         super(Config, self).ready()
