b3d961f719464495f40006fbd8d27028d715abdd
Alvaro Mariano <alvaro.lqueiroz@gmail.com>
2023-05-25 20:33:38 -0300

    Fix #289 - Added mark_unread action to the admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,10 +1,16 @@
 ''' Django notifications admin file '''
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils.translation import gettext_lazy
 from notifications.base.admin import AbstractNotificationAdmin
 from swapper import load_model
 
 Notification = load_model('notifications', 'Notification')
+
+
+def mark_unread(modeladmin, request, queryset):
+    queryset.update(unread=True)
+mark_unread.short_description = gettext_lazy('Mark selected notifications as unread')
 
 
 class NotificationAdmin(AbstractNotificationAdmin):
@@ -13,7 +19,8 @@ class NotificationAdmin(AbstractNotificationAdmin):
     list_display = ('recipient', 'actor',
                     'level', 'target', 'unread', 'public')
     list_filter = ('level', 'unread', 'public', 'timestamp',)
-    
+    actions = [mark_unread]
+
     def get_queryset(self, request):
         qs = super(NotificationAdmin, self).get_queryset(request)
         return qs.prefetch_related('actor')
