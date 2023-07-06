b154b0a499a2d6f41ee146de7abc732fd3c3a507
Alvaro Mariano <alvaro.lqueiroz@gmail.com>
2023-07-06 10:52:13 +0000

    Fix GH actions

---
+++ notifications/tests/settings_for_tests.py
@@ -0,0 +1,65 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = "secret_key"  # nosec
+
+DEBUG = True
+TESTING = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "test.sqlite3",
+    }
+}
+
+MIDDLEWARE = (
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+)
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sessions",
+    "notifications",
+]
+
+ROOT_URLCONF = "notifications.urls"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static-files")
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+LOGIN_REDIRECT_URL = "test/"
+LOGIN_URL = "/admin/login/"
+APPEND_SLASH = True
+
+DJANGO_NOTIFICATIONS_CONFIG = {
+    "USE_JSONFIELD": True,
+}
+USE_TZ = True
+
+ALLOWED_HOSTS = ["*"]
