47ed21617afbdf3e110dd9e91a512a5769da7804
Verdan Mahmood <verdan.mahmood@gmail.com>
2024-02-14 17:28:23 +0100

    Update models.py to support Django 5.x

--- notifications/base/models.py
+++ notifications/base/models.py
@@ -237,9 +237,9 @@ class AbstractNotification(models.Model):
         abstract = True
         ordering = ('-timestamp',)
         # speed up notifications count query
-        index_together = ('recipient', 'unread')
         verbose_name = _('Notification')
         verbose_name_plural = _('Notifications')
+        indexes = [models.Index(fields=["recipient", "unread"])]
 
     def __str__(self):
         ctx = {
