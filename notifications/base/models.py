61b3e6217d9f5e3afc355f3ee3d68ea49d1a7a4d
David-Mvoula <david@deplano.fr>
2024-12-05 11:38:47 +0100

    🦠fix : Update models.py by fixing index together

--- notifications/base/models.py
+++ notifications/base/models.py
@@ -237,7 +237,6 @@ class AbstractNotification(models.Model):
         abstract = True
         ordering = ('-timestamp',)
         # speed up notifications count query
-        index_together = ('recipient', 'unread')
         verbose_name = _('Notification')
         verbose_name_plural = _('Notifications')
 
